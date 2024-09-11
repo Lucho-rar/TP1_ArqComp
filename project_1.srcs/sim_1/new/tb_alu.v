34e93301091330df99b16f4c63b344b76efe5f93
Lucho-rar <lucianorodriguez131@mi.unc.edu.ar>
2024-09-11 11:04:42 -0300

    update alu

--- project_1.srcs/sim_1/new/tb_alu.v
+++ project_1.srcs/sim_1/new/tb_alu.v
@@ -1,11 +1,12 @@
 `timescale 1ns / 1ps
 
-module tb_alu;  // Testbench no tiene entradas ni salidas
+module tb_alu;  
 
-    // Definir parámetros del módulo
+    // Module parameters
     parameter NB_DATA = 4;
     parameter NB_OP = 6;
     
+    // Operations 
     localparam ADD = 6'b100000;
     localparam SUB = 6'b100010;
     localparam AND = 6'b100100;
@@ -15,28 +16,28 @@ module tb_alu;  // Testbench no tiene entradas ni salidas
     localparam SRL = 6'b000010;
     localparam NOR = 6'b100111;
     
-    integer i;
-    reg [NB_OP-1:0] op_codes[0:7]; // 7 ops
+    integer i; // Integer for index operations
+    reg [NB_OP-1:0] op_codes[0:7]; // Arrays of operations
     
-    // Declarar señales de entrada y salida para conectar con el módulo
-    reg signed [NB_DATA-1:0] i_data_a;
-    reg signed [NB_DATA-1:0] i_data_b;
-    reg [NB_OP-1:0] i_data_op;
-    wire signed [NB_DATA-1:0] o_data;
+    //Signals for testbench
+    reg signed [NB_DATA-1:0] i_data_a; // Input A
+    reg signed [NB_DATA-1:0] i_data_b; // Input B
+    reg [NB_OP-1:0] i_data_op; // Input OP
+    wire signed [NB_DATA-1:0] o_data; // Output
 
-    reg signed [NB_DATA-1:0] expected_data; // Resultado esperado para comparación
+    reg signed [NB_DATA-1:0] expected_data; // Reg for compare
 
-    // Instanciar el módulo principal (Unit Under Test)
+    // Alu instance
     alu #(.NB_DATA(NB_DATA), .NB_OP(NB_OP)) uut (
         .i_data_a(i_data_a),
         .i_data_b(i_data_b),
         .i_data_op(i_data_op),
         .o_data(o_data)
     );
-
-    // Bloque inicial para aplicar estímulos
+    
+    
     initial begin
-        
+        //Assign operations to array
         op_codes[0] = ADD;
         op_codes[1] = SUB;
         op_codes[2] = AND;
@@ -50,14 +51,11 @@ module tb_alu;  // Testbench no tiene entradas ni salidas
                  $time, i_data_a, i_data_b, i_data_op, o_data, expected_data);
                  
         for (i = 0; i < 8; i = i + 1) begin
-            // Asignar valores aleatorios a las entradas
-            i_data_a = $random % (1 << NB_DATA);  // Número aleatorio entre -8 y 7
-            i_data_b = $random % (1 << NB_DATA);  // Número aleatorio entre -8 y 7
+            i_data_a = $random % (1 << NB_DATA);  // Randon of [-8,7] range
+            i_data_b = $random % (1 << NB_DATA);  // Randon of [-8,7] range
             
-            // Seleccionar aleatoriamente una operación de las válidas
-            i_data_op = op_codes[i];
+            i_data_op = op_codes[i]; // Op code
 
-            // Calcular el resultado esperado basado en la operación
             case (i_data_op)
                 ADD: expected_data = i_data_a + i_data_b;
                 SUB: expected_data = i_data_a - i_data_b;
@@ -67,14 +65,12 @@ module tb_alu;  // Testbench no tiene entradas ni salidas
                 SRA: expected_data = i_data_a >>> i_data_b;
                 SRL: expected_data = i_data_a >> i_data_b;
                 NOR: expected_data = ~(i_data_a | i_data_b);
-                default: expected_data = 0; // caso por defecto
+                default: expected_data = 0;
             endcase
 
-            // Esperar 10 unidades de tiempo antes de cambiar los valores
-            #10;
+            #10; // Wait 10 units time 
 
-            // Chequeo automático: comparar el resultado obtenido con el esperado
-            if (o_data !== expected_data) begin
+            if (o_data !== expected_data) begin //Check response
                 $display("ERROR: A = %d, B = %d, OP = %b, Esperado = %d, Obtenido = %d",
                          i_data_a, i_data_b, i_data_op, expected_data, o_data);
             end else begin
--- project_1.srcs/sources_1/new/alu.v
+++ project_1.srcs/sources_1/new/alu.v
@@ -1,55 +1,85 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.09.2024 09:17:48
-// Design Name: 
-// Module Name: main
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module tb_alu;  
 
-module alu #
-(
-    parameter NB_DATA=  4,
-    parameter NB_OP = 6
-)
-(
-    output signed [NB_DATA-1:0] o_data, //ouput
-    input signed [NB_DATA-1:0] i_data_a, //input A
-    input signed [NB_DATA-1:0] i_data_b, //input B
-    input [NB_OP-1:0] i_data_op // input OP
+    // Module parameters
+    parameter NB_DATA = 4;
+    parameter NB_OP = 6;
     
-);
+    // Operations 
+    localparam ADD = 6'b100000;
+    localparam SUB = 6'b100010;
+    localparam AND = 6'b100100;
+    localparam OR = 6'b100101;
+    localparam XOR = 6'b100110;
+    localparam SRA = 6'b000011;
+    localparam SRL = 6'b000010;
+    localparam NOR = 6'b100111;
+    
+    integer i; // Integer for index operations
+    reg [NB_OP-1:0] op_codes[0:7]; // Arrays of operations
+    
+    //Signals for testbench
+    reg signed [NB_DATA-1:0] i_data_a; // Input A
+    reg signed [NB_DATA-1:0] i_data_b; // Input B
+    reg [NB_OP-1:0] i_data_op; // Input OP
+    wire signed [NB_DATA-1:0] o_data; // Output
 
-// tmp
-reg [NB_DATA-1:0] tmp;
+    reg signed [NB_DATA-1:0] expected_data; // Reg for compare
 
-//always
-always@(*) begin
-    case (i_data_op)
-        6'b100000: tmp = i_data_a + i_data_b; //ADD
-        6'b100010: tmp = i_data_a - i_data_b; //SUB
-        6'b100100: tmp = i_data_a & i_data_b; //AND
-        6'b100101: tmp = i_data_a | i_data_b; //OR
-        6'b100110: tmp = i_data_a ^ i_data_b; //XOR
-        6'b000011: tmp = i_data_a >>> i_data_b;
-        6'b000010: tmp = i_data_a >> i_data_b;
-        6'b100111: tmp = ~(i_data_a | i_data_b);
-        default: tmp = 0;
-    endcase
-end
+    // Alu instance
+    alu #(.NB_DATA(NB_DATA), .NB_OP(NB_OP)) uut (
+        .i_data_a(i_data_a),
+        .i_data_b(i_data_b),
+        .i_data_op(i_data_op),
+        .o_data(o_data)
+    );
+    
+    
+    initial begin
+        //Assign operations to array
+        op_codes[0] = ADD;
+        op_codes[1] = SUB;
+        op_codes[2] = AND;
+        op_codes[3] = OR;
+        op_codes[4] = XOR;
+        op_codes[5] = SRA;
+        op_codes[6] = SRL;
+        op_codes[7] = NOR;
+        
+        $monitor("Time: %0t | A = %d | B = %d | OP = %b | Result = %d | Expected = %d", 
+                 $time, i_data_a, i_data_b, i_data_op, o_data, expected_data);
+                 
+        for (i = 0; i < 8; i = i + 1) begin
+            i_data_a = $random % (1 << NB_DATA);  // Randon of [-8,7] range
+            i_data_b = $random % (1 << NB_DATA);  // Randon of [-8,7] range
+            
+            i_data_op = op_codes[i]; // Op code
 
-assign o_data = tmp;
+            case (i_data_op)
+                ADD: expected_data = i_data_a + i_data_b;
+                SUB: expected_data = i_data_a - i_data_b;
+                AND: expected_data = i_data_a & i_data_b;
+                OR: expected_data = i_data_a | i_data_b;
+                XOR: expected_data = i_data_a ^ i_data_b;
+                SRA: expected_data = i_data_a >>> i_data_b;
+                SRL: expected_data = i_data_a >> i_data_b;
+                NOR: expected_data = ~(i_data_a | i_data_b);
+                default: expected_data = 0;
+            endcase
+
+            #10; // Wait 10 units time 
+
+            if (o_data !== expected_data) begin //Check response
+                $display("ERROR: A = %d, B = %d, OP = %b, Esperado = %d, Obtenido = %d",
+                         i_data_a, i_data_b, i_data_op, expected_data, o_data);
+            end else begin
+                $display("OK: A = %d, B = %d, OP = %b, Resultado = %d", 
+                         i_data_a, i_data_b, i_data_op, o_data);
+            end
+        end
+        
+        $finish;
+    end
+
 endmodule
