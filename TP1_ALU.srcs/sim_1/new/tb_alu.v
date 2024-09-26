cf017c70bff19daf143a8cda57b3979dcef7cc62
Lucho-rar <lucianorodriguez131@mi.unc.edu.ar>
2024-09-26 20:34:25 -0300

    actualiza informe MD hasta ALU
    Signed-off-by: Lucho-rar <lucianorodriguez131@mi.unc.edu.ar>

--- TP1_ALU.srcs/sim_1/new/tb_alu.v
+++ TP1_ALU.srcs/sim_1/new/tb_alu.v
@@ -2,12 +2,12 @@
 
 module tb_alu;  
 
-    // Module parameters
-    parameter NB_DATA = 8;
+    
+    parameter NB_DATA = 8;                  // Parametros
     parameter NB_OP = 6;
     
-    // Operations 
-    localparam ADD = 6'b100000;
+                                
+    localparam ADD = 6'b100000;             // Operaciones 
     localparam SUB = 6'b100010;
     localparam AND = 6'b100100;
     localparam OR = 6'b100101;
@@ -16,14 +16,14 @@ module tb_alu;
     localparam SRL = 6'b000010;
     localparam NOR = 6'b100111;
     
-    integer i; // Integer for index operations
-    reg [NB_OP-1:0] tb_op_codes[0:7]; // Arrays of operations
+    integer i;                              // Entero auxiliar para iteraciones
+    reg [NB_OP-1:0] tb_op_codes[0:7];       // Arrays of operations
     
-    //Signals for testbench
-    reg signed [NB_DATA-1:0] tb_i_data_a; // Input A
-    reg signed [NB_DATA-1:0] tb_i_data_b; // Input B
-    reg [NB_OP-1:0] tb_i_data_op; // Input OP
-    wire signed [NB_DATA-1:0] tb_o_data; // Output
+
+    reg signed [NB_DATA-1:0] tb_i_data_a;   // Input A data
+    reg signed [NB_DATA-1:0] tb_i_data_b;   // Input B data
+    reg [NB_OP-1:0] tb_i_data_op;           // Input OP data
+    wire signed [NB_DATA-1:0] tb_o_data;    // Output data
 
     reg signed [NB_DATA-1:0] expected_data; // Reg for compare
 
@@ -37,7 +37,7 @@ module tb_alu;
     
     
     initial begin
-        //Assign operations to array
+        //Llenamos el arreglo con las operaciones
         tb_op_codes[0] = ADD;
         tb_op_codes[1] = SUB;
         tb_op_codes[2] = AND;
@@ -51,10 +51,10 @@ module tb_alu;
                  $time, tb_i_data_a, tb_i_data_b, tb_i_data_op, tb_o_data, expected_data);
                  
         for (i = 0; i < 8; i = i + 1) begin
-            tb_i_data_a = $random % (1 << NB_DATA);  // Randon of [-8,7] range
-            tb_i_data_b = $random % (1 << NB_DATA);  // Randon of [-8,7] range
+            tb_i_data_a = $random % (1 << NB_DATA);  // Randon en rango [-8,7]
+            tb_i_data_b = $random % (1 << NB_DATA);  // Randon en rango [-8,7]
             
-            tb_i_data_op = tb_op_codes[i]; // Op code
+            tb_i_data_op = tb_op_codes[i];           // Codigo de operacion
 
             case (tb_i_data_op)
                 ADD: expected_data = tb_i_data_a + tb_i_data_b;
@@ -65,12 +65,12 @@ module tb_alu;
                 SRA: expected_data = tb_i_data_a >>> tb_i_data_b;
                 SRL: expected_data = tb_i_data_a >> tb_i_data_b;
                 NOR: expected_data = ~(tb_i_data_a | tb_i_data_b);
-                default: expected_data = 0;
+                default: expected_data = {NB_DATA{1'b0}}; 
             endcase
 
-            #10; // Wait 10 units time 
+            #10; // Wait
 
-            if (tb_o_data !== expected_data) begin //Check response
+            if (tb_o_data !== expected_data) begin //Check respuesta
                 $display("ERROR: A = %d, B = %d, OP = %b, Esperado = %d, Obtenido = %d",
                          tb_i_data_a, tb_i_data_b, tb_i_data_op, expected_data, tb_o_data);
             end else begin
--- TP1_ALU.srcs/sources_1/new/alu.v
+++ TP1_ALU.srcs/sources_1/new/alu.v
@@ -1,35 +1,35 @@
 //`timescale 1ns / 1ps
 
-module alu #
-(
-    parameter NB_DATA=  8,
-    parameter NB_OP = 6
+module alu #                //parametros
+(       
+    parameter NB_DATA=  8,  //bits de datos
+    parameter NB_OP = 6     //bits de operacion  
 )
 (
-    output signed [NB_DATA-1:0] o_data, //ouput
-    input signed [NB_DATA-1:0] i_data_a, //input A
-    input signed [NB_DATA-1:0] i_data_b, //input B
-    input [NB_OP-1:0] i_data_op // input OP
+    output signed [NB_DATA-1:0] o_data, //ouput data
+    input signed [NB_DATA-1:0] i_data_a, //input A data
+    input signed [NB_DATA-1:0] i_data_b, //input B data
+    input [NB_OP-1:0] i_data_op // input OP data
     
 );
 
-// r_alu_result
-reg [NB_DATA-1:0] r_alu_result;
+
+reg [NB_DATA-1:0] r_alu_result;     //registro temporal de resultado 
 
 //always
 always@(*) begin
-    case (i_data_op)
-        6'b100000: r_alu_result = i_data_a + i_data_b; //ADD
-        6'b100010: r_alu_result = i_data_a - i_data_b; //SUB
-        6'b100100: r_alu_result = i_data_a & i_data_b; //AND
-        6'b100101: r_alu_result = i_data_a | i_data_b; //OR
-        6'b100110: r_alu_result = i_data_a ^ i_data_b; //XOR
-        6'b000011: r_alu_result = i_data_a >>> i_data_b;
-        6'b000010: r_alu_result = i_data_a >> i_data_b;
-        6'b100111: r_alu_result = ~(i_data_a | i_data_b);
-        default: r_alu_result =  {NB_DATA{1'b0}};
+    case (i_data_op)                                        //segun la operacion
+        6'b100000: r_alu_result = i_data_a + i_data_b;      //ADD
+        6'b100010: r_alu_result = i_data_a - i_data_b;      //SUB
+        6'b100100: r_alu_result = i_data_a & i_data_b;      //AND
+        6'b100101: r_alu_result = i_data_a | i_data_b;      //OR
+        6'b100110: r_alu_result = i_data_a ^ i_data_b;      //XOR
+        6'b000011: r_alu_result = i_data_a >>> i_data_b;    //SRL
+        6'b000010: r_alu_result = i_data_a >> i_data_b;     //SRA
+        6'b100111: r_alu_result = ~(i_data_a | i_data_b);   //NOR
+        default: r_alu_result =  {NB_DATA{1'b0}};           //default
     endcase
 end
 
-assign o_data = r_alu_result;
+assign o_data = r_alu_result;       //asignacion de resultado a output
 endmodule
